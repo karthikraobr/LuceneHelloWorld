5757e3f12d68ac9075cad894042f4c02159f46aa
karthikraobr <karthik.0703@gmail.com>
2017-06-11 12:58:49 +0200

    Refacactoring and code comments

--- src/HelloLucene.java
+++ src/HelloLucene.java
@@ -32,6 +32,10 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
+/**
+ * @author karth
+ *
+ */
 class Index {
 	private String lineId = "";
 	private String playName = "";
@@ -89,77 +93,108 @@ class Index {
 	}
 }
 
+/**
+ * @author karth
+ *
+ */
 public class HelloLucene {
 	public static void main(String[] args) throws IOException, XMLStreamException {
 		// Specify the analyzer for tokenizing the text.
 		// The same analyzer should be used for indexing and searching
 		SimpleAnalyzer analyzer = new SimpleAnalyzer();
-		//Use the ram directory for creating index
+		// Use the ram directory for creating index
 		Directory index = new RAMDirectory();
 		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-		//Create the index writer used to write the index.
+		// Create the index writer used to write the index.
 		IndexWriter w = new IndexWriter(index, config);
-		//Take filepath from the command line.
+		// Take filepath from the command line.
 		String filePath = args.length > 0 ? args[0] : "E:\\will_play_text.xml";
-		//Parse the XML and index it.
+		// Parse the XML and index it.
 		parseAndExtract(w, filePath);
 
-		
+		//Get handle of the reader to read from the indexes.
 		IndexReader reader = DirectoryReader.open(index);
+		//Array to store the top 1000 frequently used terms in the XML.
 		TermStats[] result;
 		List<String> terms = new ArrayList<String>();
 		try {
-			result = HighFreqTerms.getHighFreqTerms(reader, 1000, "text_entry", new HighFreqTerms.TotalTermFreqComparator());
+			//Get the top 1000 frequently used terms from the "text_entry" field. If null is passed then it queries 
+			result = HighFreqTerms.getHighFreqTerms(reader, 1000, "text_entry",
+					new HighFreqTerms.TotalTermFreqComparator());
 			int counter = 1;
+			//Log it to the console.
 			System.out.format("%10s%10s%10s\n", "No", "Text", "Frequency");
+			System.out.println("--------------------------------------------");
 			for (TermStats stats : result) {
 				System.out.format("%10d%10s%10d\n", counter, stats.termtext.utf8ToString(), stats.totalTermFreq);
+				//Store the termtext as text to generate OR and AND queries later.
 				terms.add(stats.termtext.utf8ToString());
 				counter++;
 			}
+			//Since we have to compute results for 1,2,4,8,32 and 128, we are using pow(2,x) to gernerate those numbers.
 			int raiseToPower = 0;
+			//Boilerplate for pretty printing results.
 			System.out.println(
 					"\n\n------------------------------------OR Queries------------------------------------\n\n");
 			System.out.format("%10s%20s%20s%15s%20s%20s\n", "No", "Term1", "Term2", "N", "Time Taken", "Result Count");
+			System.out.println(
+					"----------------------------------------------------------------------------------------------------------");
+			//For a BooleanQuery with no MUST clauses one or more SHOULD clauses must match a document for the BooleanQuery to match. (OR queries)
 			Occur occur = BooleanClause.Occur.SHOULD;
+			//List to hold the times to calculate the average.
 			List<Double> times = new ArrayList<Double>();
+			//Iterating 200 times the first 100 to generate OR queries and the next 100 to generate AND queries. 
 			for (int i = 0; i < 200; i++) {
+				//Once we encounter index 100 it is time to switch to AND queries since we have already generated 100 OR queries.
 				if (i == 100) {
+					//Pretty print.
 					System.out.format("%10s%20s%20s%15s%20s%20s\n", "", "", "", "", "---------", "");
 					System.out.format("%10s%20s%20s%15s%20f%20s\n", "", "", "", "",
 							times.stream().mapToDouble(Double::doubleValue).sum() / 100, "");
 					times = new ArrayList<Double>();
+					//Use this operator for clauses that must appear in the matching documents. (AND queries)
 					occur = BooleanClause.Occur.MUST;
 					System.out.println(
 							"\n\n------------------------------------AND Queries------------------------------------\n\n");
 					System.out.format("%10s%20s%20s%15s%20s%20s\n", "No", "Term1", "Term2", "N", "Time Taken",
 							"Result Count");
+					System.out.println(
+							"----------------------------------------------------------------------------------------------------------");
 					raiseToPower = 0;
 				}
-				
+
+				//Get 2 random terms from a list of top 1000 frequently used terms. 
 				String term1 = terms.get(getRandomIndex(terms.size()));
 				String term2 = terms.get(getRandomIndex(terms.size()));
-				
+
+				//Create a boolean query.
 				BooleanQuery.Builder categoryQuery = getQuery(occur, term1, term2);
+				//For n=1,2,4,8,32,128
 				int hitsPerPage = (int) Math.pow(2, raiseToPower);
+				//Initialize the searcher.
 				IndexSearcher searcher = new IndexSearcher(reader);
 				double startTime = System.nanoTime();
+				//Query the index for the query generated.
 				TopDocs docs = searcher.search(categoryQuery.build(), hitsPerPage);
 				double currentTime = System.nanoTime();
 				// Get the elapsed time in milliseconds
 				double timeTaken = (currentTime - startTime) / 1000000;
 				times.add(timeTaken);
 				ScoreDoc[] hits = docs.scoreDocs;
+				//Log the results to the console.
 				System.out.format("%10d%20s%20s%15d%20f%20d\n", i + 1, term1, term2, hitsPerPage, timeTaken,
 						hits.length);
+				//We need to query from n =1,2,4,8,32,and 128, hence we reset the variable to 0 once we reach 128 which is pow(2,7) 
 				if (raiseToPower == 7) {
 					raiseToPower = 0;
 				} else {
 					raiseToPower++;
 				}
+				//We don't need pow(2,4) and pow(2,6)
 				if (raiseToPower == 4 || raiseToPower == 6) {
 					raiseToPower++;
 				}
+				//Log average time taken for all the AND queries to the console.
 				if (i == 199) {
 					System.out.format("%10s%20s%20s%15s%20s%20s\n", "", "", "", "", "---------", "");
 					System.out.format("%10s%20s%20s%15s%20f%20s\n", "", "", "", "",
@@ -171,9 +206,20 @@ public class HelloLucene {
 			e.printStackTrace();
 		}
 
+		//Close the reader.
 		reader.close();
 	}
 
+	/**
+	 * @param w
+	 *            The index writer being used to write to the index.
+	 * @param filePath
+	 *            The path of the xml file.
+	 * @throws FactoryConfigurationError
+	 * @throws FileNotFoundException
+	 * @throws XMLStreamException
+	 * @throws IOException
+	 */
 	private static void parseAndExtract(IndexWriter w, String filePath)
 			throws FactoryConfigurationError, FileNotFoundException, XMLStreamException, IOException {
 		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
@@ -186,55 +232,61 @@ public class HelloLucene {
 		Index idx = new Index();
 		while (xmlEventReader.hasNext()) {
 			XMLEvent xmlEvent = xmlEventReader.nextEvent();
+			//Check if the tag is a start element and check if it it a column tag
+			if (xmlEvent.isStartElement() && xmlEvent.asStartElement().getName().getLocalPart().equals("column")) {
+				Iterator<?> iterator = xmlEvent.asStartElement().getAttributes();
 
-			if (xmlEvent.isStartElement()) {
-
-				if (xmlEvent.asStartElement().getName().getLocalPart().equals("column")) {
-					Iterator<?> iterator = xmlEvent.asStartElement().getAttributes();
-
-					while (iterator.hasNext()) {
-						Attribute attribute = (Attribute) iterator.next();
-						String name = attribute.getValue();
-						xmlEvent = xmlEventReader.nextEvent();
-						if (xmlEvent.isCharacters()) {
-							String value = xmlEvent.asCharacters().getData() != null ? xmlEvent.asCharacters().getData()
-									: "";
-							switch (name) {
-							case "line_id":
-								idx.setLineId(value);
-								break;
-							case "play_name":
-								idx.setPlayName(value);
-								break;
-							case "speech_number":
-								idx.setSpeechNumber(value);
-								break;
-							case "line_number":
-								idx.setLineNumber(value);
-								break;
-							case "speaker":
-								idx.setSpeaker(value);
-								break;
-							case "text_entry":
-								idx.setTextEntry(value);
-								break;
-							default:
-								break;
-							}
+				while (iterator.hasNext()) {
+					Attribute attribute = (Attribute) iterator.next();
+					String name = attribute.getValue();
+					xmlEvent = xmlEventReader.nextEvent();
+					if (xmlEvent.isCharacters()) {
+						//Get the value inside a tag.
+						String value = xmlEvent.asCharacters().getData() != null ? xmlEvent.asCharacters().getData()
+								: "";
+						//Switch based on the attribute value and set it to the model we created.
+						switch (name) {
+						case "line_id":
+							idx.setLineId(value);
+							break;
+						case "play_name":
+							idx.setPlayName(value);
+							break;
+						case "speech_number":
+							idx.setSpeechNumber(value);
+							break;
+						case "line_number":
+							idx.setLineNumber(value);
+							break;
+						case "speaker":
+							idx.setSpeaker(value);
+							break;
+						case "text_entry":
+							idx.setTextEntry(value);
+							break;
+						default:
+							break;
 						}
-
 					}
 
 				}
+			//If an end element of type table is encountered we have parsed one document and it is now safe to write it to the index. 
 			} else if (xmlEvent.isEndElement() && xmlEvent.asEndElement().getName().getLocalPart().equals("table")
 					&& idx.getPlayName() != null) {
 				addToIndex(w, idx);
 				idx = new Index();
 			}
 		}
+		//Close the writer.
 		w.close();
 	}
 
+	/**
+	 * @param occur Specify whether you need an AND or OR query.
+	 * @param term1 The first term to query.
+	 * @param term2 The second term to query.
+	 * @return The generated query.
+	 */
 	private static BooleanQuery.Builder getQuery(Occur occur, String term1, String term2) {
 		BooleanQuery.Builder categoryQuery = new BooleanQuery.Builder();
 		TermQuery catQuery1 = new TermQuery(new Term("text_entry", term1));
